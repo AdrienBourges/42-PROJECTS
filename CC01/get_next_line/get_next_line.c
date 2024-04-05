e0573eb3e30b3ae8c55848f56786be90eb5c31b1
Adriano <adrienupmc@gmail.com>
2024-04-05 17:39:24 +0200

    algo fonctionnel

--- CC01/get_next_line/get_next_line.c
+++ CC01/get_next_line/get_next_line.c
@@ -2,6 +2,8 @@
 
 int ft_is_newline(char *line) // fct qui permet de detecter si on a une newline dans notre char *, auquel cas on arrete de read dans ft_stock_text
 {
+	if (!line)
+		return (0);
 	while (*line != '\n')
 	{
 		if (!*line)
@@ -9,13 +11,15 @@ int ft_is_newline(char *line) // fct qui permet de detecter si on a une newline 
 		line++;
 	}
 	return (1);
-}
+}	
 
 size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
 	i = 0;
+	if (!s)
+		return (0);
 	while (s[i])
 		i++;
 	return (i);
@@ -27,14 +31,16 @@ char *ft_merge(char *s1, char *s2)
 	size_t	total_size;
 	int		i;
 	int		j;
-
+	
+	if (!s1)
+		return (s2);
 	total_size = ft_strlen(s1) + ft_strlen(s2);
 	result = malloc(total_size + 1);
 	if (!result)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s1[i])
+		while (s1[i])
 	{
 		result[i] = s1[i];
 		i++;
@@ -45,8 +51,7 @@ char *ft_merge(char *s1, char *s2)
 		j++;
 	}
 	result[i + j] = '\0';
-	free(s1);
-	free(s2);
+	//free(s1);
 	return (result);
 }
 char *ft_stock_text(int fd, char *line) // fonction qui va permettre de stocker dynamiquement le texte dans la limite du buffer_size disponible, si buffer_size > nb de carac on stock tout directement car line est vide au premier appel de fonction
@@ -61,13 +66,13 @@ char *ft_stock_text(int fd, char *line) // fonction qui va permettre de stocker 
 		byte_index = read(fd, temp, BUFFER_SIZE);
 		if (byte_index == -1)
 		{
-			free(temp);
+		//	free(temp);
 			return (NULL);
 		}
 		temp[byte_index] = '\0';
 		line = ft_merge(line, temp);
 	}
-	free(temp);
+//	free(temp);
 	return (line);
 }
 
@@ -77,11 +82,58 @@ char *ft_append_line(char *line) // fonction qui sert a ajouter la ligne dans un
 	int i;
 
 	i = 0;
-
+	while (line[i] != '\n')
+	{
+		i++;
+	}
+	result = malloc (i + 2);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (line[i] != '\n' && line[i])
+	{
+		result[i] = line[i];
+		i++;
+	}
+	if (line[i + 1])
+	{
+		result[i] = '\n';
+		result[i + 1] = '\0';
+	}
+	else
+		result[i] = '\0';
+	return (result);
 }
 
 char *ft_clear_line(char *str_temp) // fonction pour clean le static string jusquau premier \n rencontre  avant le prochain appel de gnl
 {
+	char *result;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str_temp[i] != '\n' && str_temp[i])
+	{
+		i++;
+	}
+	i++;
+	j = i;
+	while (str_temp[j])
+	{
+		j++;
+	}
+	result = malloc (j + 1);
+	j = 0;
+	while (str_temp[i])
+	{
+		result[j] = str_temp[i];
+		i++;
+		j++;
+	}
+	result[j] = '\0';
+//	free(str_temp);
+	return (result);
 }
 
 char *get_next_line(int fd)
@@ -89,12 +141,12 @@ char *get_next_line(int fd)
 	char *result;
 	static char *str_temp;
 
-	str_temp = NULL;
-
 	if (fd <= 0 || BUFFER_SIZE < 0)
 		return (NULL);
 	str_temp = ft_stock_text(fd, str_temp);
-	return (str_temp);
+	result = ft_append_line(str_temp);
+	str_temp = ft_clear_line(str_temp);
+	return (result);
 
 
 
@@ -110,6 +162,7 @@ char *get_next_line(int fd)
 	   if (!temp)
 	   return (NULL);
 	   read_check = read(fd, temp, BUFFER_SIZE);
+	j = 0;
 	   temp[read_check] = '\0';
 	   while (temp[i] != '\n' && i < read_check)
 	   {
@@ -159,13 +212,13 @@ char *get_next_line(int fd)
 		int		fd1;
 		fd1 = open("./txtfile.txt", O_RDONLY);
 		i = 1;
-		//while (i < 4)
-		//{
+		while (i < 5)
+		{
 			line = get_next_line(fd1);
-			printf("ligne %d: %s", i, line);
+			printf("ligne %d:%s", i, line);
 			free(line);
-		//	i++;
-		//}
+			i++;
+		}
 		close(fd1);
 		return (0);
 	}
